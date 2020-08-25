f48f4aba6fa6f7ad864d4f3e06b9a0b11dfb79ce
shadyvhs <56814593+shadyvhs@users.noreply.github.com>
2020-08-26 00:16:40 +0530

    Updated topic for laserscan

--- catkin_ws/src/motion_plan/scripts/bug2.py
+++ catkin_ws/src/motion_plan/scripts/bug2.py
@@ -102,7 +102,7 @@ def main():
 
     rospy.init_node('bug2')
 
-    sub_laser = rospy.Subscriber('/i214/laser/scan', LaserScan, clbk_laser)
+    sub_laser = rospy.Subscriber('/camera/scan', LaserScan, clbk_laser)
     sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
     sub_imu = rospy.Subscriber('/imu', Imu, clk_yaw)
 
