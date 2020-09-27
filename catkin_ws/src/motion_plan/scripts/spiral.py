252524e438bf78f12f01fdf7321751467c33e927
shadyvhs <56814593+shadyvhs@users.noreply.github.com>
2020-09-27 15:49:32 +0530

    Spiral Search

---
+++ catkin_ws/src/motion_plan/scripts/spiral.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+
+import sys,select
+import rospy
+import time
+import math
+import numpy as np
+from geometry_msgs.msg import Point
+from darknet_ros_msgs.msg import BoundingBoxes
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from scipy.stats import uniform
+from scipy.stats import levy
+from sensor_msgs.msg import Imu
+from tf import transformations
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
+from geometry_msgs.msg import Twist, Point
+from std_srvs.srv import *
+
+pi=3.14159265
+global detected
+detected=False
+
+def clbk_darknet(msg):
+	global detected
+	detected=True
+	print("In callback rn")   #If this function is called, object has been detected
+
+rospy.init_node('spiral')
+rospy.wait_for_service('/gazebo/set_model_state')
+srv_client_set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+model_state = ModelState()
+model_state.model_name = 'i214'
+model_state.pose.position.x = 0
+model_state.pose.position.y = 0
+resp = srv_client_set_model_state(model_state)
+
+rate = rospy.Rate(10)
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+sub_darknet = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, clbk_darknet)
+v=0.5
+w=10
+twist_msg = Twist()
+
+while not rospy.is_shutdown():
+	twist_msg.linear.x=0.5
+	twist_msg.angular.z=w
+	pub.publish(twist_msg)
+	if detected==True:
+		v=0
+		w=0
+		twist_msg.linear.x=v
+		twist_msg.angular.z=w
+		pub.publish(twist_msg)
+		sys.exit()
+	time.sleep(2*pi/w)
+	#subsequent code imposes different conditions on changes in angular velocity to ensure more uniform changes in radius
+	if w<0.8 and w>0.65:
+		w=w*0.95	#95% of angular velocity
+	elif w<=0.65:
+		w=w*0.99	#99% of angular velcocity
+	else:
+		w=w*0.9		#90 % of angular velocity
+	if detected==True:
+		v=0
+		w=0
+		twist_msg.linear.x=v
+		twist_msg.angular.z=w
+		pub.publish(twist_msg)
+		sys.exit()
+		
+
