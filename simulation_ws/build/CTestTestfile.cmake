5eb03c128fbe0858421a3f2e83487ca2868d0da2
Arun <d.aarun@yahoo.in>
2020-09-27 19:40:34 +0530

    Added build and devel to gitignore

--- simulation_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kevin/autonav_sim/simulation_ws/src
-# Build directory: /home/kevin/autonav_sim/simulation_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("i214_description")
--- simulation_ws/build/i214_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kevin/autonav_sim/simulation_ws/src/i214_description
-# Build directory: /home/kevin/autonav_sim/simulation_ws/build/i214_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
